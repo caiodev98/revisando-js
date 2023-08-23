73697353861e091a49c6b271cd809447989a3003
Caio Leal <110993322+caiodev98@users.noreply.github.com>
2023-08-23 18:20:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+/* 1- Crie um script que exiba a mensagem
+ "Hello World!" em um alerta no navegador. */
+
+// alert("Hello World")
+
+/* 2- Crie um script que declare duas 
+variáveis e exiba o resultado da soma 
+entre elas. */
+
+/* let numberOne = prompt("Digite um número")
+let numberTwo = prompt("Digite outro número:")
+
+numberOne = Number(numberOne)
+numberTwo = Number(numberTwo)
+
+alert(`A soma é: ${numberOne + numberTwo}`) */
+
+/* 3- Crie um script que declare uma variável e
+ verifique se o seu valor é um número. 
+ Se for, exiba a mensagem "É um número", 
+caso contrário, exiba a mensagem 
+"Não é um número". */
+
+/* let variavel = 10;
+
+if(typeof variavel == 'number'){
+  alert(`É um número`)
+}else{
+  alert(`Não é um número`)
+} */
+
+/* 4- Crie um script que declare uma 
+variável e verifique se o seu valor é 
+uma string. Se for, exiba a mensagem 
+"É uma string", caso contrário, 
+exiba a mensagem "Não é uma string". */
+
+/* let variavel = "caio";
+
+if(typeof variavel == 'string'){
+  alert(`É uma string`)
+}else{
+  alert(`Não é uma string`)
+} */
+
+/* 5- Crie um script que declare uma 
+variável e verifique se o seu valor é um 
+booleano. Se for, exiba a mensagem 
+"É um booleano", caso contrário, 
+exiba a mensagem "Não é um booleano". */
+
+/* let variavel = false;
+
+if(typeof variavel == 'boolean'){
+  alert(`É um booleano`)
+}else{
+  alert(`Não é um booleano`)
+} */
+
+/* 6- Crie um script que declare 
+duas variáveis e 
+exiba o resultado da subtração entre elas. */
+
+/* const number1 = 10
+const number2 = 7
+const sub = number1 - number2
+
+alert(sub) */
+
+/* 7- Crie um script 
+que declare duas variáveis e exiba 
+o resultado da multiplicação entre elas. */
+
+/* const number1 = 10
+const number2 = 7
+const mult = number1 * number2
+
+alert(mult) */
+
+/* 8- Crie um script que declare duas 
+variáveis e exiba o resultado da divisão entre elas. */
+
+/* const number1 = 10
+const number2 = 7
+const div = number1 / number2
+
+alert(div) */
+
+/* 9- Crie um script que declare uma variável e verifique se o seu 
+valor é um número par. Se for, exiba a mensagem 
+"É um número par", caso contrário, exiba a mensagem "Não é um número par". */
+
+/* const number = 2
+
+if(number % 2 == 0){
+  alert(`É par`)
+}else{
+  alert(`É impar`)
+} */
+
+/* 10- Crie um script que declare uma variável e verifique se o 
+seu valor é um número ímpar. Se for, exiba a mensagem 
+"É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar". */
+
+const number = 7
+
+if(number % 2 != 0){
+  alert(`É impar`)
+}else{
+  alert(`É par`)
+}
